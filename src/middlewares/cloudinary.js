746d5570252780366d86d2afa6d7a554ae2b38c7
unknown <shumbushoemilef@gmail.com>
2023-01-23 19:24:17 +0200

    remove-unneccessary-code

--- src/middlewares/cloudinary.js
+++ src/middlewares/cloudinary.js
@@ -1,11 +1,11 @@
-const cloudinary = require('cloudinary').v2;
-import 'dotenv/config';
+import cloudinary from "cloudinary";
+import "dotenv/config";
 
-// Configuration 
+// Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 module.exports = cloudinary;
