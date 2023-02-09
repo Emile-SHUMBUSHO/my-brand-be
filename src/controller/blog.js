91e194273ee1b6f0f312f5f74bc2647838107214
unknown <shumbushoemilef@gmail.com>
2023-02-09 14:48:07 +0200

    improve blog response status

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -13,63 +13,87 @@ export const createBlog = async (req, res) => {
     const savedBlog = await blog.save();
     res
       .status(status.CREATED)
-      .json({ message: "Blog created successfully", blog:savedBlog });
+      .json({ message: "Blog created successfully", blog: savedBlog });
   } catch (error) {
-    return res.status(status.BAD_REQUEST).json({ message: error.message });
+    res.status(status.SERVER_ERROR).json({ message: error.message });
   }
 };
 
-export const allBlogs = (req, res) => {
+export const allBlogs = async (req, res) => {
   try {
-    Blog.find((error, blogs) => {
-      if (error) {
-        res.status(status.SERVER_ERROR).json(error);
-      } else {
-        res.json(blogs);
-      }
+    const Allblogs = await Blog.find({}).sort({
+      date: -1,
     });
+
+    if (Allblogs.length < 1) {
+      res.status(status.NOT_FOUND).json({
+        error: "No Blog Found",
+        counts: Allblogs.length,
+      });
+    } else {
+      res.status(status.OK).json({
+        message: "All Blog Found",
+        counts: Allblogs.length,
+        data: Allblogs,
+      });
+    }
   } catch (error) {
-    return res.status(status.SERVER_ERROR).json(error);
+    res.status(status.SERVER_ERROR).json(error);
   }
 };
 
-export const singleBlog = (req, res) => {
+export const singleBlog = async (req, res) => {
   try {
-    Blog.findById(req.params.id, (error, blog) => {
-      if (error) res.send(error);
-      res.json(blog);
-    });
-  } catch {}
-};
-
-export const updateBlog = (req, res) => {
-  try {
-    Blog.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-        description: req.body.description,
-        blogBody: req.body.blogBody,
-        imageUrl: req.body.imageUrl,
-      },
-      { new: true },
-      (error, blog) => {
-        if (error) res.send(error);
-        res.json(blog);
-      }
-    );
-  } catch (err) {
-    return res.status(status.SERVER_ERROR).json(err);
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res
+        .status(status.NOT_FOUND)
+        .json({ error: `Blog with id ${req.params.id} not found` });
+    } else {
+      res.status(status.OK).json({
+        message: "blog retrieved successfully",
+        data: blog,
+      });
+    }
+  } catch (error) {
+    res.status(status.SERVER_ERROR).json({ error: `Internal server error ${error}` });
   }
 };
 
-export const deleteBlog = (req, res) => {
+export const updateBlog = async (req, res) => {
   try {
-    Blog.findByIdAndRemove(req.params.id, { new: true }, (error, blog) => {
-      if (error) res.send(error);
-      res.json(blog);
+    if (!(await Blog.findById(req.params.id))) {
+      return res
+        .status(status.NOT_FOUND)
+        .json({ error: `Blog with id ${req.params.id} not found` });
+    }
+    const updateBlog = await Blog.findByIdAndUpdate(req.params.id, req.body);
+    res.status(status.CREATED).json({
+      message: "Blog updated successfully",
+      data: updateBlog,
     });
   } catch (error) {
-    console.log(error);
+    res
+      .status(status.SERVER_ERROR)
+      .json({ error: `Internal server error ${error}` });
+  }
+};
+
+export const deleteBlog = async (req, res) => {
+  try {
+    if (!(await Blog.findById(req.params.id))) {
+      return res
+        .status(status.NOT_FOUND)
+        .json({ error: `Blog with id ${req.params.id} not found` });
+    }
+    const deleletBlog = await Blog.findByIdAndRemove(req.params.id);
+    res.status(status.OK).json({
+      message: "Blog deleted successfully",
+      deleteBlog: deleletBlog,
+    });
+  } catch (error) {
+    res
+      .status(status.SERVER_ERROR)
+      .json({ error: `Internal server error ${error}` });
   }
 };
